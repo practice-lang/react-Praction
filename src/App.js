b9fbd283c1f5a97c67e3b792eddaa33ab850c556
ttakkku <k2005118@gmail.com>
2019-05-22 22:40:03 +0900

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Greet from './components/Greet'
+import Welcome from './components/Welcome'
+
+function App() {
+  return (
+    <div className="App">
+      <Greet/>
+      <Welcome/>
+    </div>
+  );
+}
+
+export default App;
+
+// @Next Study
+// https://www.youtube.com/watch?v=oecI26cWqzk&list=PLC3y8-rFHvwgg3vaYJgHGnModB54rxOk3&index=7 
